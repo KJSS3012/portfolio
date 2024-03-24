b98ff27b5cc964e89b370f5133809bb82ac5cff0
Kaique Santos <kaique30122004@gmail.com>
2024-03-24 18:24:16 -0300

    section-initial 1.3.1

--- src/components/Buttons/GreenButton/index.js
+++ src/components/Buttons/GreenButton/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function GreenButton(props) {
   return (
     <a
-      className="w-2/5 md:w-1/3 flex items-center justify-centern text-center bg-custom-green border-2 border-custom-green px-4 md:px-6 py-2 md:py-4 rounded text-white hover:text-custom-green hover:bg-transparent text-lg font-semibold duration-300"
+      className="w-2/5 md:w-1/3 flex items-center justify-center text-center bg-custom-green border-2 border-custom-green px-4 md:px-6 py-2 md:py-4 rounded text-white hover:text-custom-green hover:bg-transparent text-lg font-semibold duration-300"
       href={props.href}
     >
       {props.title}
