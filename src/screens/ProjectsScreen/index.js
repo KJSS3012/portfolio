d117208af5e1f549bbf0d3e43157dff572e3989c
Kaique Santos <kaique30122004@gmail.com>
2024-07-20 18:35:20 -0300

    json vercel test

--- src/screens/ProjectsScreen/index.js
+++ src/screens/ProjectsScreen/index.js
@@ -1,7 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { ContainerProjects, SectionTitle } from "../../components";
 
 function ProjectsScreen() {
+  const [isTeste, setTeste] = useState("");
+
+  useEffect(() => {
+    const fetchTeste = async () => {
+      try {
+        const response = await fetch("/assets/json/skills.json");
+        const data = await response.json();
+        setTeste(data["teste"]);
+      } catch (error) {
+        console.error("Error fetching the skills.json:", error);
+      }
+    };
+    fetchTeste();
+  }, []);
+
   return (
     <Fragment>
       <ContainerProjects>
@@ -15,6 +30,7 @@ function ProjectsScreen() {
           Olá! Infelizmente, esta parte do site, assim como muitas outras, ainda
           não está finalizada. Caso queira acompanhar a evolução, convido-o a me
           seguir no GitHub ou LinkedIn.
+          {isTeste}
         </p>
       </ContainerProjects>
     </Fragment>
